a3d50267d7940a4f4b50fc3e1180fbd2c97aeebc
Elsayed Elbeshry <elsayedelbeshry119@gmail.com>
2024-10-25 00:32:54 +0300

    List passwords, Update password and delete password

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -7,59 +7,68 @@ use Illuminate\Http\Request;
 
 class PasswordController extends Controller
 {
+
+    // TODO correct the docs here, return ...
     /**
      * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return void
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        return $request->user()->passwords;
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Store a newly created resource in storage
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
-    }
+        $validated = $request->validate([
+            'platform_name' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
+            'encrypted_password' => 'required|string|max:30',
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Password $password)
-    {
-        //
-    }
+        $password = $request->user()->passwords()->create($validated);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Password $password)
-    {
-        //
+        return response()->json($password);
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Password $password
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, Password $password)
     {
-        //
+        $validated = $request->validate([
+            'platform_name' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
+            'encrypted_password' => 'required|string|max:30',
+        ]);
+
+        $password->update($validated);
+
+        return response()->json($password);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param Password $password
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Password $password)
     {
-        //
+        $password->delete();
+
+        return response()->json(['message' => 'Password deleted']);
     }
 }
