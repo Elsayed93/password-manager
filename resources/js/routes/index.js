4d10468fcb3d6846b28394bc9f331b8f0c153e41
Elsayed Elbeshry <elsayedelbeshry119@gmail.com>
2024-10-27 08:29:16 +0300

    Prevent un-authenticated user from enter passwords page

--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -2,17 +2,45 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../components/Login.vue';
 import Register from '../components/Register.vue';
 import Passwords from '../components/Passwords.vue';
+import { useAuthStore } from '../store/authStore';
 
 const routes = [
-    { path: '/', component: Login },
-    { path: '/login', component: Login },
+    {
+        path: '/',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
     { path: '/register', component: Register },
-    { path: '/passwords', component: Passwords },
+    {
+        path: '/passwords',
+        component: Passwords,
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory('/'),
     routes,
 });
+
+router.beforeEach((to, from) => {
+    const authStore = useAuthStore();
+
+    // explicitly return false to cancel the navigation
+    if (
+        // make sure the user is authenticated
+        !authStore.user &&
+        // ❗️ Avoid an infinite redirect
+        to.name !== 'Login'
+    ) {
+        // redirect the user to the login page
+        return { name: 'Login' }
+    }
+    return true
+})
 
 export default router;
